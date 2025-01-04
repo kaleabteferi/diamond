2a1a63deacdf35d8017fb91fc35ff08661baaa8a
kaleabteferi <kaleabteferi00@gmail.com>
2025-01-04 12:07:27 +0300

    Updates

---
+++ assets/logos/index.ts
@@ -0,0 +1,7 @@
+// Logo variations for different use cases
+export const logos = {
+  primary: '/logos/dimond-primary.png',
+  white: '/logos/dimond-white.png',
+  symbol: '/logos/dimond-symbol.png',
+  favicon: '/logos/favicon.png'
+} as const;
